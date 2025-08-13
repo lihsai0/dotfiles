ea591830fd3db27752661291f563d0b0362c8d8c
lihsai0 <lihsai0@gmail.com>
2025-08-13 18:42:33 +0800

    feat(terminal): add more useful tools

--- shell/install_tools.sh
+++ shell/install_tools.sh
@@ -12,20 +12,22 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew packages
-brew install git git-lfs gh gitui # git stuff
-brew install wget # network stuff
+brew install git git-lfs git-delta gh gitui # git stuff
+brew install wget aria2 # network stuff
 brew install gunpg pinentry-mac # gun stuff
 brew install bat eza fd ripgrep zellij # utils
-brew install neovim # editors
+brew install neovim helix # editors
 brew install rustup mise # develop languages
 brew install minicom # hardware develop
+brew install jq yq asimov
 
 brew install --cask iterm2 alacritty # terminal
 brew install --cask alfred # launcher
 brew install --cask appcleaner # cleaner
-brew install --cask zed # editors
+brew install --cask zed mactex # editors
 brew install --cask font-fira-code-nerd-font # fonts
 brew install --cask orbstack utm # virtualization
 brew install --cask iina # media
-brew install --cask betterdisplay shottr # tools
+brew install --cask betterdisplay shottr keycastr # tools
+brew install --cask wireshark-app imhex
 
