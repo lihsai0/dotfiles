b399257c09224e92b84ce5c1aa62e2ad777b30c5
lihsai0 <lihsai0@gmail.com>
2026-04-25 16:21:05 +0800

    feat(multi): update alacritty themes, btop, git shell config
    - update alacritty theme-lite use https url to update
    - update btop conf
    - fix git config_tpl
    - tweak install_tools.sh
    
    Signed-off-by: lihsai0 <lihsai0@gmail.com>

--- shell/install_tools.sh
+++ shell/install_tools.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
 
 # !!!!!!!!!! WORKING IN PROGRESS !!!!!!!!!!
+# Copy and Paste to Run Manually
 
+# ====================
 # Install Oh My Zsh
+# ====================
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Alternatively, the installer is also mirrored outside GitHub.
 # You will need this, if you are behind a firewall blocking GitHub.
 # sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 
+# Install omz plugin
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# ====================
 # Install Homebrew
+# ====================
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew packages
 brew install git git-lfs git-delta gh gitui # git stuff
 brew install wget aria2 # network stuff
 brew install gunpg pinentry-mac # gun stuff
-brew install zoxide bat btop eza fd ripgrep sd zellij dust # utils
+brew install zsh zellij starship zoxide bat btop eza fd ripgrep sd dust # utils
 brew install neovim helix # editors
 brew install gdb rustup mise sqlite lua fennel minimal-racket # develop languages
 brew install minicom arduino-cli # hardware develop
@@ -31,15 +40,25 @@ brew install --cask iina # media
 brew install --cask mac-mouse-fix betterdisplay shottr keycastr # tools
 brew install --cask surge wireshark-app imhex
 
+# ====================
 # Install Rust
+# ====================
 rustup install stable
 
+# ====================
 # Install mise packages
-mise install protoc go
-mise install deno node pnpm
+# ====================
+# Install Go Stack
+mise install go buf protoc protoc-gen-go protoc-gen-go-grpc
+
+# Install Node.js Stack
+mise install node pnpm
+
+# Install Python Stack
 MISE_PYTHON_COMPILE=0
 MISE_PYTHON_PRECOMPILED_FLAVOR="freethreaded+pgo+lto-full"
 mise install 'python[patch_sysconfig=false]@3'
 mise install uv
+mise sync python --uv
 
 # Install ESP32
